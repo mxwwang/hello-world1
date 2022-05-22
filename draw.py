76c727407eb14773c86b92b6002385a03c5a1304
mxwwang <xw415@nyu.edu>
2022-05-22 12:24:53 +0800

    add new python files

---
+++ draw.py
@@ -0,0 +1,13 @@
+import turtle
+painter = turtle.Turtle()
+<<<<<<< HEAD
+
+for _ in range(18): #usually i ; just a dummy, a placeholder & dont care about its value
+	painter.forward(100) #tab/3 spaces(old)/4 spaces(new)
+	painter.left(100) #all inditation shoule be CONSISTANT!
+	
+=======
+painter.forward(100)
+painter.left(90)
+>>>>>>> parent of 1cbbe75 (repeat 4 times with a for loop)
+turtle.done()
